080304daaa2903ae1186de348b54f640c99ef11a
Gulafshan <gulafshan@geniuslabs.live>
2024-03-12 10:34:05 +0530

    episode_02

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
 // const heading = React.createElement("h1", {id: "heading"}, "hello world from react sonu////// "); 
 // console.log("heading",heading); // object
 // const root = ReactDOM.createRoot(document.getElementById("root"));
